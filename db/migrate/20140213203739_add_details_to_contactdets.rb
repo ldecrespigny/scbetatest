aac7b370fd217343f6990113242498145454086a
Lachlan de Crespigny <ldecrespigny@gmail.com>
2014-02-13 17:14:36 -0800

    changed html on cover page

--- db/migrate/20140213203739_add_details_to_contactdets.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToContactdets < ActiveRecord::Migration
-  def change
-    add_column :contactdets, :cell, :sting
-    add_column :contactdets, :contactemail, :string
-    add_column :contactdets, :employees, :string
-    add_column :contactdets, :function, :string
-  end
-end
